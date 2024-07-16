4a6d099bea4ad7e59239b340c760e9bd644a8eeb
Sher <siaufui327@hotmail.com>
2024-07-16 13:31:21 +0800

    add database to common.py

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -67,6 +67,16 @@ INTERNAL_IPS = [
     # ...
 ]
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront3',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Taraislove@88'
+    }
+}
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001'
