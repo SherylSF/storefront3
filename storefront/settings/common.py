c17da847ed3d89e95dfe3ce7ec270ffa704c444d
Sher <siaufui327@hotmail.com>
2024-07-16 17:06:05 +0800

    add port

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -71,7 +71,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
-        'HOST': '127.0.01',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'Taraislove@88'
     }
--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -11,6 +11,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
         'HOST': '127.0.0.1',
+        'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'Taraislove@88'
     }
