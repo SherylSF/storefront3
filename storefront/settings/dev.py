657ffdfe6260bb663b3c7c3425e8ca5e389c3bfa
Sher <siaufui327@hotmail.com>
2024-07-20 19:25:12 +0800

    changed host to 127.0.0.1

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -10,7 +10,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'storefront3',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'Taraislove@88'
